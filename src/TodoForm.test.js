1432d47a3266748479c971c6740be55c7ec3280a
YeongSeon1 <tjdudtjs1101@gmail.com>
2022-02-22 03:19:51 +0000

    Duplicated code modification in setup

--- src/TodoForm.test.js
+++ src/TodoForm.test.js
@@ -15,14 +15,14 @@ describe('<TodoForm />', () => {
     }
 
     it('has input and a button', () => {
-        const {getByText, getByPlaceholderText} = render(<TodoForm />);
-        getByPlaceholderText('할 일을 입력하세요~'); //input 있는지 없는지
-        getByText('등록'); // button 버튼이 있는지
+        const {input, button} = setup();
+
+        expect(input).toBeTruthy();
+        expect(button).toBeTruthy();
     });
 
     it('change input', () => {
-        const {getByPlaceholderText} = render(<TodoForm />);
-        const input = getByPlaceholderText('할 일을 입력하세요~');
+        const {input} = setup();
         fireEvent.change(input, {target: {value: 'TDD 배우기'}}); //value 대신에 id를 넣을 수도 있고 다른 것도 넣을 수 있다.
         expect(input).toHaveAttribute('value', 'TDD 배우기'); //value에 TDD 배우기가 있는지 검사
     });
@@ -30,9 +30,7 @@ describe('<TodoForm />', () => {
 
     it('calls onInsert and clears input', () => {
         const onInsert = jest.fn();
-        const { getByText, getByPlaceholderText } = render(<TodoForm onInsert={onInsert} />);
-        const input = getByPlaceholderText('할 일을 입력하세요~');
-        const button = getByText('등록');
+        const {input, button} = setup({onInsert});
         fireEvent.change(input, {target: {value: 'TDD 배우기'}});
         fireEvent.click(button);
         expect(onInsert).toBeCalledWith('TDD 배우기');
