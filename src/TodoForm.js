75dfc132e1f4d287f9e47c2c2a0f992abd1dc2b0
YeongSeon1 <tjdudtjs1101@gmail.com>
2022-02-22 02:46:59 +0000

    TodoForm Submit event test OK

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,16 +1,21 @@
 import React, { useCallback, useState } from 'react';
 
-const TodoForm = () => {
+const TodoForm = ({onInsert}) => {
 
     const [value, setValue] = useState('');
     const onChange = useCallback(e => {
         setValue(e.target.value);
     }, []);
 
+    const onSubmit = useCallback(e =>{
+        onInsert(value);
+        setValue('');
+        e.preventDefault();
+    }, [onInsert, value]);
 
     return (
         <div>
-            <form>
+            <form onSubmit={onSubmit}>
                 <input placeholder='할 일을 입력하세요~' value = {value} onChange = {onChange} /> {/* 파란색 value는 HTML 속성이고 {} 안에 value는 useState에 있는 value이다. */}
                 <button type='submit'>등록</button>
             </form>
--- src/TodoForm.test.js
+++ src/TodoForm.test.js
@@ -15,6 +15,18 @@ describe('<TodoForm />', () => {
         fireEvent.change(input, {target: {value: 'TDD 배우기'}}); //value 대신에 id를 넣을 수도 있고 다른 것도 넣을 수 있다.
         expect(input).toHaveAttribute('value', 'TDD 배우기'); //value에 TDD 배우기가 있는지 검사
     });
+    // 여기까지는 키보드로 입력했을 때 테스트
+
+    it('calls onInsert and clears input', () => {
+        const onInsert = jest.fn();
+        const { getByText, getByPlaceholderText } = render(<TodoForm onInsert={onInsert} />);
+        const input = getByPlaceholderText('할 일을 입력하세요~');
+        const button = getByText('등록');
+        fireEvent.change(input, {target: {value: 'TDD 배우기'}});
+        fireEvent.click(button);
+        expect(onInsert).toBeCalledWith('TDD 배우기');
+        expect(input).toHaveAttribute('value', '');
+    });
     
     
 });
