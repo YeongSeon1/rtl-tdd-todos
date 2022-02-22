47c8d5f87e652c672a429b0bb8d5ecc5f4c024ea
YeongSeon1 <tjdudtjs1101@gmail.com>
2022-02-22 07:20:17 +0000

    TodoApp TodoForm TodoList render test OK

--- src/TodoApp.test.js
+++ src/TodoApp.test.js
@@ -3,10 +3,17 @@ import React from 'react';
 import TodoApp from './TodoApp';
 
 describe('<TodoApp />', () => {
+
     it('renders TodoForm and TodoList', () => {
         const {getByText, getByTestId} = render(<TodoApp />);
         getByText('등록');
         getByTestId('TodoList');
+    });
+
+    it('renders two defaults todos', () => {
+        const {getByText} = render(<TodoApp />);
+        getByText('TDD 배우기');
+        getByText('react-testing-library 사용하기');
     });
     
 });
