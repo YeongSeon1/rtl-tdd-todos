2cfc2a8765e87b9df68bdc34d53576f48b984460
YeongSeon1 <tjdudtjs1101@gmail.com>
2022-02-22 02:28:11 +0000

    TodoForm Input Test

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 
 const TodoForm = () => {
+
+    const [value, setValue] = useState('');
+    const onChange = useCallback(e => {
+        setValue(e.target.value);
+    }, []);
+
+
     return (
         <div>
             <form>
-                <input placeholder='할 일을 입력하세요~' />
+                <input placeholder='할 일을 입력하세요~' value = {value} onChange = {onChange} /> {/* 파란색 value는 HTML 속성이고 {} 안에 value는 useState에 있는 value이다. */}
                 <button type='submit'>등록</button>
             </form>
         </div>
--- src/TodoForm.test.js
+++ src/TodoForm.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react'; // react-testing-library 이건 옛날 방식
+import { fireEvent, render } from '@testing-library/react'; // react-testing-library 이건 옛날 방식
 import TodoForm from './TodoForm';
 
 describe('<TodoForm />', () => {
@@ -8,6 +8,14 @@ describe('<TodoForm />', () => {
         getByPlaceholderText('할 일을 입력하세요~'); //input 있는지 없는지
         getByText('등록'); // button 버튼이 있는지
     });
+
+    it('change input', () => {
+        const {getByPlaceholderText} = render(<TodoForm />);
+        const input = getByPlaceholderText('할 일을 입력하세요~');
+        fireEvent.change(input, {target: {value: 'TDD 배우기'}}); //value 대신에 id를 넣을 수도 있고 다른 것도 넣을 수 있다.
+        expect(input).toHaveAttribute('value', 'TDD 배우기'); //value에 TDD 배우기가 있는지 검사
+    });
+    
     
 });
 
